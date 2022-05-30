8d5595748dccc5aa576f60c9335f111deefd2c8f
cdt96 <cody.theroux3@gmail.com>
2022-05-30 12:04:04 -0400

    started a saveBtn function in js

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,2 +1,7 @@
 // This will be the date and time displaying to the ID currentDay using moment.js that I linked in my html
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+
+//This will be the function that makes the save button save the text and time that text is logged for 
+$(".saveBtn").on("click", function(){
+   
+})
