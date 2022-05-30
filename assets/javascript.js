3f0f70da4089e8abd1c1ff8502841ea64a370869
cdt96 <cody.theroux3@gmail.com>
2022-05-30 18:43:15 -0400

    added statements to pull the info out of local storage and also changed how i arranged text and hour in my setItem for local storage

--- assets/javascript.js
+++ assets/javascript.js
@@ -3,12 +3,21 @@ $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
 var containerEl = $(".container");
 
 //This will be the function that makes the save button save the text and time that text is logged for 
-$(".saveBtn").on("click", function(){
+$(".saveBtn").on("click", function () {
     //assigning the variables text and hour to the information under id in html and the information in the inserttext classes in html
-  var text= containerEl.find(".inserttext").val();
-  var hour = $(this).parent().attr("id");
-  //sending the above variables to localstorage when the save button is clicked
-  localStorage.setItem(text, hour);
-  
-  
+    var text = containerEl.find(".inserttext").val();
+    var hour = $(this).parent().attr("id");
+    //sending the above variables to localstorage when the save button is clicked
+    localStorage.setItem(hour, text);
+    console.log(hour);
 })
+// this will pull what is saved in local storage and place it in the correct hour row on the page
+$("#hour9 .inserttext").val(localStorage.getItem("hour9"))
+$("#hour10 .inserttext").val(localStorage.getItem("hour10"))
+$("#hour11 .inserttext").val(localStorage.getItem("hour11"))
+$("#hour12 .inserttext").val(localStorage.getItem("hour12"))
+$("#hour13 .inserttext").val(localStorage.getItem("hour13"))
+$("#hour14 .inserttext").val(localStorage.getItem("hour14"))
+$("#hour15 .inserttext").val(localStorage.getItem("hour15"))
+$("#hour16 .inserttext").val(localStorage.getItem("hour16"))
+$("#hour17 .inserttext").val(localStorage.getItem("hour17"))
