a7d5f0b6f8ca80968075523b1067b46c1f34bc11
cdt96 <cody.theroux3@gmail.com>
2022-05-30 12:36:16 -0400

    set my variables in my saveBtn function

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,7 +1,12 @@
 // This will be the date and time displaying to the ID currentDay using moment.js that I linked in my html
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+var containerEl = $(".container");
 
 //This will be the function that makes the save button save the text and time that text is logged for 
 $(".saveBtn").on("click", function(){
-   
+  var text= containerEl.find(".inserttext").val();
+  var hour = $(this).parent().attr("id");
+  console.log(hour)
+  
+  
 })
