5cd1e1638fc225b7c03f7930974ffda0a1b50032
cdt96 <cody.theroux3@gmail.com>
2022-05-31 12:32:40 -0400

    started my function for tracking the current hour

--- assets/javascript.js
+++ assets/javascript.js
@@ -13,11 +13,33 @@ $(".saveBtn").on("click", function () {
 })
 // this will pull what is saved in local storage and place it in the correct hour row on the page
 $("#hour9 .inserttext").val(localStorage.getItem("hour9"))
-$("#hour10 .inserttext").val(localStorage.getItem("hour10"))
-$("#hour11 .inserttext").val(localStorage.getItem("hour11"))
-$("#hour12 .inserttext").val(localStorage.getItem("hour12"))
-$("#hour13 .inserttext").val(localStorage.getItem("hour13"))
-$("#hour14 .inserttext").val(localStorage.getItem("hour14"))
-$("#hour15 .inserttext").val(localStorage.getItem("hour15"))
-$("#hour16 .inserttext").val(localStorage.getItem("hour16"))
-$("#hour17 .inserttext").val(localStorage.getItem("hour17"))
+$("#hour10 .inserttext").val(localStorage.getItem("hour10"));
+$("#hour11 .inserttext").val(localStorage.getItem("hour11"));
+$("#hour12 .inserttext").val(localStorage.getItem("hour12"));
+$("#hour13 .inserttext").val(localStorage.getItem("hour13"));
+$("#hour14 .inserttext").val(localStorage.getItem("hour14"));
+$("#hour15 .inserttext").val(localStorage.getItem("hour15"));
+$("#hour16 .inserttext").val(localStorage.getItem("hour16"));
+$("#hour17 .inserttext").val(localStorage.getItem("hour17"));
+
+//this will be the function that tracks the hour we are in and declare it the past, present, or future
+function hourTracker () {
+    var currentTime = moment().hours();
+    $(".inserttext").each(function () {
+        var otherTime = ($(this).parent().attr("id"));
+       if (currentTime > otherTime) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }if (currentTime < otherTime) {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } if (currentTime = otherTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+    });
+}
+hourTracker();
